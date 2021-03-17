1703688eff2e9099477aec1a5cb9a57bfaaf3b0c
Derrick Zhang <derrick.zhang@crypto.com>
2021-03-17 10:55:47 +0800

    Support SSM

--- aws-env.go
+++ aws-env.go
@@ -2,31 +2,46 @@ package main
 
 import (
 	"context"
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
 )
 
 func main() {
-	if os.Getenv("AWS_SM_ID") == "" || os.Getenv("AWS_SM_REGION") == "" {
-		log.Println("aws-env running locally, without AWS_SM_ID and AWS_SM_REGION")
-		return
+	region := ""
+	if os.Getenv("AWS_REGION") != "" {
+		region = os.Getenv("AWS_SM_REGION")
+	} else if os.Getenv("AWS_SM_REGION") != "" {
+		region = os.Getenv("AWS_SM_REGION")
 	}
-	secretId := os.Getenv("AWS_SM_ID")
-	region := os.Getenv("AWS_SM_REGION")
 
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
 	if err != nil {
 		log.Fatalf("failed to load SDK configuration, %v", err)
 	}
 
-	client := secretsmanager.NewFromConfig(cfg)
+	if os.Getenv("AWS_SM_ID") != "" {
+		secretId := os.Getenv("AWS_SM_ID")
+		client := secretsmanager.NewFromConfig(cfg)
+		ExportVariablesFromSecretsManager(client, secretId)
+	} else if os.Getenv("AWS_ENV_PATH") != "" {
+		path := os.Getenv("AWS_ENV_PATH")
+		client := ssm.NewFromConfig(cfg)
+		ExportVariablesFromSSM(client, path, true, "")
+	} else {
+		log.Println("aws-env running locally")
+		return
+	}
+}
+
+func ExportVariablesFromSecretsManager(client *secretsmanager.Client, secretId string) {
 	input := &secretsmanager.GetSecretValueInput{
 		SecretId: aws.String(secretId),
 	}
@@ -36,23 +51,44 @@ func main() {
 		log.Fatalf("failed to get secret values, %v", err)
 	}
 
-	var secretString string
 	if result.SecretString != nil {
-		secretString = *result.SecretString
-	} else {
-		decodedBinarySecretBytes := make([]byte, base64.StdEncoding.DecodedLen(len(result.SecretBinary)))
-		len, err := base64.StdEncoding.Decode(decodedBinarySecretBytes, result.SecretBinary)
-		if err != nil {
-			log.Panic("Base64 Decode Error:", err)
-			return
+		secretString := *result.SecretString
+		var envars map[string]interface{}
+		json.Unmarshal([]byte(secretString), &envars)
+
+		for key, value := range envars {
+			fmt.Printf("export %s=$'%s'\n", key, value)
 		}
-		secretString = string(decodedBinarySecretBytes[:len])
+	}
+}
+
+func ExportVariablesFromSSM(client *ssm.Client, path string, recursive bool, nextToken string) {
+	input := &ssm.GetParametersByPathInput{
+		Path:           aws.String(path),
+		WithDecryption: true,
+		Recursive:      recursive,
 	}
 
-	var envars map[string]interface{}
-	json.Unmarshal([]byte(secretString), &envars)
+	if nextToken != "" {
+		input.NextToken = aws.String(nextToken)
+	}
 
-	for key, value := range envars {
-		fmt.Printf("export %s=$'%s'\n", key, value)
+	result, err := client.GetParametersByPath(context.TODO(), input)
+	if err != nil {
+		log.Fatalf("failed to get paremters by path, %v", err)
+	}
+
+	for _, parameter := range result.Parameters {
+		name := *parameter.Name
+		value := *parameter.Value
+
+		env := strings.Replace(strings.Trim(name[len(path):], "/"), "/", "_", -1)
+		value = strings.Replace(value, "\n", "\\n", -1)
+
+		fmt.Printf("export %s=$'%s'\n", env, value)
+	}
+
+	if result.NextToken != nil {
+		ExportVariablesFromSSM(client, path, recursive, *result.NextToken)
 	}
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/aws/aws-sdk-go-v2 v1.2.1
 	github.com/aws/aws-sdk-go-v2/config v1.1.2
 	github.com/aws/aws-sdk-go-v2/service/secretsmanager v1.1.2
+	github.com/aws/aws-sdk-go-v2/service/ssm v1.1.2
 )
